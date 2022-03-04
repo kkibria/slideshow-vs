399cfb3e2fcd665de7b6278f4e154f0849e27eb3
Khan Kibria <khan.kibria@gmail.com>
2022-03-04 01:13:33 -0800

    update

--- src/extension.ts
+++ src/extension.ts
@@ -16,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('slideshow-vs.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from slideshow!');
+		vscode.window.showInformationMessage('Hello vscode');
 	});
 
 	context.subscriptions.push(disposable);
